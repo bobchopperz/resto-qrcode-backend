eae1f94c1f9cc3818d8bb1909ce24b46b0be3509
bobchopperz <bobchopperz@users.noreply.github.com>
2025-11-02 15:19:54 +0700

    ready for pushing edit opsi

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Delete, UseGuards, HttpCode, HttpStatus } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { JwtAuthGuard } from '../user/jwt-auth.guard';
 
 @Controller('order')
 export class OrderController {
@@ -16,7 +17,20 @@ export class OrderController {
         @Param('year') year: number,
         @Param('month') month: number,
     ) {
-        // This will call a new method in your service
         return this.orderService.findByMonth(+year, +month);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async remove(@Param('id') id: string): Promise<void> {
+        return this.orderService.remove(id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post(':id/resend-whatsapp')
+    @HttpCode(HttpStatus.OK)
+    async resendWhatsapp(@Param('id') id: string) {
+        return this.orderService.resendWhatsappMessage(id);
     }
 }
