f0e45d9682179f12a2700489a0e9c529bb8ec4ad
bobchopperz <bobchopperz@users.noreply.github.com>
2025-11-04 17:05:59 +0700

    debugging for export whatsappforwarding module

--- src/whatsapp-config/whatsapp-config.module.ts
+++ src/whatsapp-config/whatsapp-config.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { WhatsappConfigController } from './whatsapp-config.controller';
 import { WhatsappConfigService } from './whatsapp-config.service';
-import { Config, ConfigSchema } from './whatsapp-config.schema'; // Impor dari schema lokal
+import { Config, ConfigSchema } from './whatsapp-config.schema';
 import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
-    // Nama fitur tetap 'Config' sesuai nama kelas skema
     MongooseModule.forFeature([{ name: Config.name, schema: ConfigSchema }]),
     UserModule,
   ],
   controllers: [WhatsappConfigController],
   providers: [WhatsappConfigService],
+  exports: [WhatsappConfigService], // Tambahkan baris ini untuk mengekspor service
 })
 export class WhatsappConfigModule {}
